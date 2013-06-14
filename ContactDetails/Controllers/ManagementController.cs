c5828924680eaf60c014ad66cff2565a659c1a26
Ruddy <ruddy@bbd.co.za>
2013-06-14 07:31:12 +0200

    first commit

---
+++ ContactDetails/Controllers/ManagementController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment3Test.Areas.ContactDetails.Models;
+
+namespace Assignment3Test.Areas.ContactDetails.Controllers
+{
+    public class ManagementController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }        
+
+        [HttpPost]
+        public ActionResult Index(Assignment3Test.Areas.ContactDetails.Models.ContactDetails details)
+        {
+            return View(details);
+        }     
+    }
+}
